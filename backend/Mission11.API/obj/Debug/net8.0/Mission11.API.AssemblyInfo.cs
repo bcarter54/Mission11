2d560b29f14c529b767b70b72f66881f6539c827
bcarter54 <carterbryce54@gmail.com>
2025-03-21 22:36:13 -0600

    Moved sln file

--- backend/Mission11.API/obj/Debug/net8.0/Mission11.API.AssemblyInfo.cs
+++ backend/Mission11.API/obj/Debug/net8.0/Mission11.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3d0714e20a3c142992f2a53ac85135614657d2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+968ed21e02d3293438eaaef4f1c439ac521b4850")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission11.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
